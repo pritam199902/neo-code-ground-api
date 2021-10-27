78a1597860fffa756ff18004d726d88cfd192548
Pritam <www.pritamdas1999@gmail.com>
2021-10-28 02:41:22 +0530

    source up 02

--- database/connection/db_connection.js
+++ database/connection/db_connection.js
@@ -3,20 +3,20 @@ const mongoose = require('mongoose');
 const db_config = require('../config/db_config.json')
 
 const connectDB = async () => {
-    try {
-      await mongoose.connect(
-        db_config.db_url_dev,
-        {
-          useNewUrlParser: true, 
-          useUnifiedTopology: true
-        }
-      );
-  
-      console.log('DB connected successfully!');
-    } catch (err) {
-      console.error("DB connection error : ",err.message);
-      process.exit(1);
-    }
-  };
+  try {
+    await mongoose.connect(
+      db_config.db_url_prod,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
-  module.exports = connectDB
+    console.log('DB connected successfully!');
+  } catch (err) {
+    console.error("DB connection error : ", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB
